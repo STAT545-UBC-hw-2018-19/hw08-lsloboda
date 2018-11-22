d39823df92952c11938d3df7f46b66f1a687c96a
Laura Sloboda <laura.sloboda@gmail.com>
2018-11-21 23:29:14 -0800

    final

--- bcl/app.R
+++ bcl/app.R
@@ -1,62 +1,78 @@
 library(shiny)
-library(ggplot2)
-library(dplyr)
+library(tidyverse)
 library(DT)
 
 bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 
+
 ui <- fluidPage(
-  titlePanel("BC Liquor Store prices"),
-  sidebarLayout(
-    sidebarPanel(
-    	img(src = "drinks.jpg", align = "right", width = 250, height = 100),
-    	sliderInput("priceInput", "Price", 0, 100, c(25, 40), pre = "$"),
-      radioButtons("typeInput", "Product type",
-                  choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
-                  selected = "WINE"),
-      uiOutput("countryOutput")
-    ),
-    
-    mainPanel(
-    	img(src = "bcl-image.png", align = "right", width = 550, height = 55),
-    	plotOutput("coolplot"),
-      br(), br(),
-      tableOutput("results")
-    )
-)
+	titlePanel("BC Liquor price app", 
+						 windowTitle = "BCL app"),
+	sidebarLayout(
+		sidebarPanel(
+			#img(src = "drinks.jpg", align = "right", width = 250, height = 100),
+			sliderInput("priceInput", "Select your desired price range.",
+									min = 0, max = 100, value = c(15, 30), pre="$"),
+			radioButtons("typeInput", "Select your alcoholic beverage type.", 
+									 choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+									 selected = "WINE"),
+			uiOutput("countryOutput"),
+			checkboxInput("sortInput", "Sort by ascending price?",
+										value = FALSE),
+			conditionalPanel(condition = "input.typeInput == 'WINE'",
+											 sliderInput("wineInput", "Select desired sweetness level:",
+											 						min = 0, max = 10, value = c(0, 10))
+			)
+		),
+		mainPanel(
+			img(src = "bcl-image.png", align = "right", width = 550, height = 55),
+			plotOutput("coolplot"),
+			DT::dataTableOutput("bcl_data")
+		)
+	)
 )
 
 server <- function(input, output) {
-  output$countryOutput <- renderUI({
-    selectInput("countryInput", "Country",
-                sort(unique(bcl$Country)),
-                selected = "CANADA")
-  })  
-  
-  filtered <- reactive({
-    if (is.null(input$countryInput)) {
-      return(NULL)
-    }    
-    
-    bcl %>%
-      filter(Price >= input$priceInput[1],
-             Price <= input$priceInput[2],
-             Type == input$typeInput,
-             Country == input$countryInput
-      )
-  })
-  
-  output$coolplot <- renderPlot({
-    if (is.null(filtered())) {
-      return()
-    }
-    ggplot(filtered(), aes(Alcohol_Content)) +
-      geom_histogram()
-  })
-
-  output$results <- renderTable({
-    filtered()
-  })
-}
+	output$countryOutput <- renderUI({
+		selectInput("countryInput", "Country",
+								sort(unique(bcl$Country)),
+								selected = "CANADA")
+	})
+	
+	bcl_price <- reactive({
+		if (input$sortInput) {
+			bcl %>% 
+				filter(Price < input$priceInput[2],
+							 Price > input$priceInput[1],
+							 Type == input$typeInput)  %>%
+				arrange(Price)
+			
+		} else {
+			bcl %>% 
+				filter(Price < input$priceInput[2],
+							 Price > input$priceInput[1],
+							 Type == input$typeInput)       
+		}
+	})
+	bcl_sweet <- reactive({
+		if (input$typeInput == 'WINE') {
+			bcl_price() %>% 
+				filter(Type == 'WINE',
+							 Sweetness <= input$wineInput[2],
+							 Sweetness >= input$wineInput[1])
+		} else {
+			bcl_price()
+		}
+	})
+	output$coolplot <- renderPlot({
+		bcl_sweet() %>% 
+			ggplot(aes(Price)) +
+			geom_histogram()
+	})
+	output$bcl_data <- DT::renderDataTable({
+		bcl_sweet()
+	})
+	
+		}
 
 shinyApp(ui = ui, server = server)
